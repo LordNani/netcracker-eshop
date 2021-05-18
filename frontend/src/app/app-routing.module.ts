c3dd6e354b6d4c4e1ba2142686a22240604d8b55
Marina Montag <montag.marinka@gmail.com>
2021-05-18 20:48:50 +0300

    comment all places where non-existing components are used

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {MainPageComponent} from './Components/main-page/main-page.component';
 import {LoginComponent} from './Components/authorization/login/login.component';
 import {RegistrationComponent} from './Components/authorization/registration/registration.component';
-import {AuctionsComponent} from './Components/auctions/auctions.component';
-import {ProductsComponent} from './Components/products/products.component';
-import {ShoppingCartComponent} from './Components/shopping-cart/shopping-cart.component';
-import {SettingsComponent} from './Components/settings/settings.component';
-import {ProductComponent} from './Components/product/product.component';
-import {AuctionComponent} from './Components/auction/auction.component';
-import {BidComponent} from './Components/bid/bid.component';
+// import {AuctionsComponent} from './Components/auctions/auctions.component';
+// import {ProductsComponent} from './Components/products/products.component';
+// import {ShoppingCartComponent} from './Components/shopping-cart/shopping-cart.component';
+// import {SettingsComponent} from './Components/settings/settings.component';
+// import {ProductComponent} from './Components/product/product.component';
+// import {AuctionComponent} from './Components/auction/auction.component';
+// import {BidComponent} from './Components/bid/bid.component';
 
 const routes: Routes = [
   {
@@ -29,35 +29,35 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegistrationComponent
-  },
-  {
-    path: 'auctions',
-    component: AuctionsComponent
-  },
-  {
-    path: 'auction',
-    component: AuctionComponent
-  },
-  {
-    path: 'bid',
-    component: BidComponent
-  },
-  {
-    path: 'products',
-    component: ProductsComponent
-  },
-  {
-    path: 'product',
-    component: ProductComponent
-  },
-  {
-    path: 'shopping-cart',
-    component: ShoppingCartComponent
-  },
-  {
-    path: 'settings',
-    component: SettingsComponent
   }
+  // {
+  //   path: 'auctions',
+  //   component: AuctionsComponent
+  // },
+  // {
+  //   path: 'auction',
+  //   component: AuctionComponent
+  // },
+  // {
+  //   path: 'bid',
+  //   component: BidComponent
+  // },
+  // {
+  //   path: 'products',
+  //   component: ProductsComponent
+  // },
+  // {
+  //   path: 'product',
+  //   component: ProductComponent
+  // },
+  // {
+  //   path: 'shopping-cart',
+  //   component: ShoppingCartComponent
+  // },
+  // {
+  //   path: 'settings',
+  //   component: SettingsComponent
+  // }
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,25 +4,25 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MainPageComponent } from './Components/main-page/main-page.component';
 import {AuthorizationModule} from './Components/authorization/authorization.module';
-import { ProductsComponent } from './Components/products/products.component';
-import { AuctionsComponent } from './Components/auctions/auctions.component';
-import { ShoppingCartComponent } from './Components/shopping-cart/shopping-cart.component';
-import { SettingsComponent } from './Components/settings/settings.component';
-import { ProductComponent } from './Components/product/product.component';
-import { AuctionComponent } from './Components/auction/auction.component';
-import { BidComponent } from './Components/bid/bid.component';
+// import { ProductsComponent } from './Components/products/products.component';
+// import { AuctionsComponent } from './Components/auctions/auctions.component';
+// import { ShoppingCartComponent } from './Components/shopping-cart/shopping-cart.component';
+// import { SettingsComponent } from './Components/settings/settings.component';
+// import { ProductComponent } from './Components/product/product.component';
+// import { AuctionComponent } from './Components/auction/auction.component';
+// import { BidComponent } from './Components/bid/bid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    ProductsComponent,
-    AuctionsComponent,
-    ShoppingCartComponent,
-    SettingsComponent,
-    ProductComponent,
-    AuctionComponent,
-    BidComponent,
+    // ProductsComponent,
+    // AuctionsComponent,
+    // ShoppingCartComponent,
+    // SettingsComponent,
+    // ProductComponent,
+    // AuctionComponent,
+    // BidComponent,
   ],
   imports: [
     BrowserModule,
