b5102be452c032740d2cbb810aac614c159c9f92
Marina Montag <montag.marinka@gmail.com>
2021-05-19 18:56:49 +0300

    validation of login form

--- frontend/src/app/_model/user.ts
+++ frontend/src/app/_model/user.ts
@@ -1,4 +1,6 @@
 export class User{
+  constructor() {
+  }
   firstName: string;
   lastName: string;
   email: string;
--- frontend/src/app/authorization/login/login.component.ts
+++ frontend/src/app/authorization/login/login.component.ts
@@ -10,7 +10,8 @@ import {HttpErrorResponse} from '@angular/common/http';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginUserData: User;
+  loginUserData = new User();
+  error = false;
   constructor(private router: Router,
               private authService: AuthService) { }
   ngOnInit(): void {
@@ -20,10 +21,12 @@ export class LoginComponent implements OnInit {
     console.log(this.loginUserData);
     this.authService.loginUser(this.loginUserData).subscribe(
       res => {
-        localStorage.setItem('token', res.token);
+        this.error = false;
+        localStorage.setItem('token', res.headers.get('Authorization').body);
         this.router.navigate(['/main']);
       },
       error => {
+        this.error = true;
         if (error instanceof HttpErrorResponse) {
           if (error.status === 401) {
             console.log(error); // ?????
